f457a28a6c81aa845fdf1ac582d79e7ea053474f
Astrico <astrico.aulia@gmail.com>
2017-09-29 08:42:13 +0700

    add tests

--- serverhealth/tests.py
+++ serverhealth/tests.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+from .views import getSystemHealthData
+
+class ViewTest(TestCase):
+	def test_get_data(self):
+		self.assertIsNotNone(getSystemHealthData())
+
+	def test_get_index(self):
+		response = self.client.get('/serverhealth/')
+		self.assertEqual(response.status_code, 200)
